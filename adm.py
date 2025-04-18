f690d30529749a27c714f4566aa70f798916de2f
adminliu-main <admshao.24@outlook.com>
2025-04-18 22:27:03 +0800

    全球 每个州 损耗 入口 出口 生产 按照年份求和为y 年份为x出图

--- adm.py
+++ adm.py
@@ -11,7 +11,7 @@ def jiancha(data1:pd.DataFrame):
     data=pd.DataFrame(data1)
     if isinstance(data, pd.DataFrame) and data.empty:
         raise EmptyDataFrameError("提供的 pandas.core.frame.DataFrame 为空")
-    return(data)
+    return(data1)
 
 def unique_values_columns(data1:pd.DataFrame,name:str):
     data=jiancha(data1)
@@ -46,8 +46,48 @@ def year_back(data1:pd.DataFrame,name:str=None):
         year_list.append(str(name))        
     return(year_list)
         
+def linechart(chutu:int,x_data:list,y_data:list,ziti:str=None,title_name:str=None,X_name:str=None,Y_name:str=None,x_size:int=None,y_size:int=None):
+    result = {
+        'x': x_data,
+        'y': y_data
+    }
+    result1 = pd.DataFrame(result)
+    # 字体问题 我一般使用mac编程 所以为 黑体STHeiti 宋体STSong
+    # 如果为windows 请使用 黑体SimHei 宋体SimSun
+    if ziti is None:
+        ziti = 'STHeiti'
+    plt.rcParams['font.family'] = ziti
+    plt.rcParams.update({'font.size': 10})
+    # 解决负号显示问题
+    plt.rcParams['axes.unicode_minus'] = False  
+    if x_size==None:
+        x_size=30
+    if y_size==None:
+        y_size=10
+    plt.figure(figsize=(x_size, y_size))
+    sns.lineplot(x='x', y='y', data=result1)
 
-def show_result_line(data:pd.DataFrame,name:str,title_name:str=None,X_name:str=None,Y_name:str=None,x_size:int=None,y_size:int=None):
+    for i in range(len(y_data)):
+        plt.text(x_data[i], y_data[i], str(y_data[i]), ha='center', va='bottom')
+
+    if title_name==None:
+        title_name=""
+    plt.title(title_name)
+    if X_name==None:
+        X_name="Year"
+    plt.xlabel(X_name)
+    if Y_name==None:
+        Y_name="value"
+    plt.ylabel(Y_name)
+    ax = plt.gca()
+    ax.invert_yaxis()
+    if chutu==0:
+        fig = plt.gcf()
+        fig.savefig(f'{title_name}.png')
+    else:
+        plt.show()
+
+def old_show_result_line(data:pd.DataFrame,name:str,title_name:str=None,X_name:str=None,Y_name:str=None,x_size:int=None,y_size:int=None):
     data['Country'] = data['Country'].str.strip()
     data = data.reset_index(drop=True)
     country_indices = data[data['Country']== name].index
@@ -94,10 +134,21 @@ def show_result_line(data:pd.DataFrame,name:str,title_name:str=None,X_name:str=N
 def sum_year_data(data:pd.DataFrame,year1:int=None,region_num:int=None,region:str=None):
     region_data=unique_value_column_back(data,'Region')#从给的dataframe中取独特值
     state_group=data.groupby("Region")#按照地区分组
-    if region == None:
-        if region_num==None:
-            region_num=0
-    state_1=state_group.get_group(region_data[region_num])#取出需要的地区的相关表   
+    if region is None:
+        if region_num is None:
+            region_num = 0
+        if region_num < 0 or region_num >= len(region_data):
+            print(f"region_num {region_num} 超出了 region_data 的索引范围。")
+            return None
+        selected_region = region_data[int(region_num)]
+    else:
+        selected_region = region
+    try:
+        state_1 = state_group.get_group(selected_region)  # 取出需要的地区的相关表
+    except KeyError:
+        print(f"未找到键为 {selected_region} 的组。")
+        return None
+ 
     year_num=year_back(state_1)
     year_num_series = pd.Series(year_num)
     #print(year_num_series)
@@ -115,10 +166,21 @@ def sum_year_data(data:pd.DataFrame,year1:int=None,region_num:int=None,region:st
 def mean_year_data(data:pd.DataFrame,year1:int=None,region_num:int=None,region:str=None):
     region_data=unique_value_column_back(data,'Region')#从给的dataframe中取独特值
     state_group=data.groupby("Region")#按照地区分组
-    if region == None:
-        if region_num==None:
-            region_num=0
-    state_1=state_group.get_group(region_data[region_num])#取出需要的地区的相关表   
+    if region is None:
+        if region_num is None:
+            region_num = 0
+        if region_num < 0 or region_num >= len(region_data):
+            print(f"region_num {region_num} 超出了 region_data 的索引范围。")
+            return None
+        selected_region = region_data[int(region_num)]
+    else:
+        selected_region = region
+    try:
+        state_1 = state_group.get_group(selected_region)  # 取出需要的地区的相关表
+    except KeyError:
+        print(f"未找到键为 {selected_region} 的组。")
+        return None
+ 
     year_num=year_back(state_1)
     year_num_series = pd.Series(year_num)
     #print(year_num_series)
@@ -130,4 +192,20 @@ def mean_year_data(data:pd.DataFrame,year1:int=None,region_num:int=None,region:s
         return
     mean_by_region=state_1[year].mean()
     return(mean_by_region)
+
+        
     
+
+
+def show_result_sum(chutu:int,data:pd.DataFrame,weishu:int,ziti:str,title:int=None,diqu:int=None):
+    year_list=year_back(data)
+    sum_list=[]
+    for i in year_list:
+        resulut=sum_year_data(data,int(i),diqu)
+        if resulut is not None:
+            sum_list.append(resulut)
+    #print(type(sum_list))
+    rounded_numbers = [round(num, weishu) for num in sum_list]
+    if title is None:
+        title="求和结果"
+    linechart(chutu,year_back(data,),rounded_numbers,ziti,title)
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ row_names = ['']
 #可能需要去重
 
 data = pd.read_csv(file_path)
-num_col=data.columns[2:]
+num_col=data.columns[3:]
 for col in num_col:
     data[col]=pd.to_numeric(data[col],errors="coerce") #coerce返回NAN raise返回异常 ignore忽略
 
@@ -29,12 +29,33 @@ net_generation_data=data[data['Features'] == 'net generation']
 #ad.show_result_line(loss_data,"Afghanistan")
 
 
-
+#ad.unique_values_columns(loss_data,'Region')
 #region_data=ad.unique_value_column_back(loss_data,'Region')
 #state_group=loss_data.groupby("Region")
 #state_1=state_group.get_group(region_data[0])
-
+#ad.unique_values_columns(data,'Region')
 #sum_by_region=state_1[ad.year_back(state_1)[0]].sum()
 #mean_by_region=state_1[ad.year_back(state_1)[0]].mean()
-print(ad.sum_year_data(loss_data,1999))
-print(ad.mean_year_data(loss_data,1999))
+
+#print(ad.mean_year_data(loss_data,1999))
+
+
+region_data=ad.unique_value_column_back(data,'Region')#从给的dataframe中取独特值
+data2=net_generation_data
+i1=0
+name2="_net_generation"
+xuhao="4_"
+for i1 in range(0,7):
+    name=xuhao+region_data[i1]+name2
+    ad.show_result_sum(0,data2,1, "STHeiti",name,i1)
+
+ad.show_result_sum(0,data2,1, "STHeiti",f'{xuhao}global_{name2}')
+
+
+
+#ad.show_result_sum(0,imports_data,1, "STHeiti","global_import")
+#ad.show_result_sum(0,exports_data,1, "STHeiti","global_export")
+#ad.show_result_sum(0,net_generation_data,1, "STHeiti","global_net")
+# 字体问题 我一般使用mac编程 所以为 黑体STHeiti 宋体STSong
+# 如果为windows 请使用 黑体SimHei 宋体SimSun
+
