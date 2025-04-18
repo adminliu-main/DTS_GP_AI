6b5aadb413b0b6b747ed804b06ce15c9180d08f5
adminliu <admshao.24@outlook.com>
2025-04-18 20:24:18 +0800

    增加缺失值处理，平均&求和方法

--- adm.py
+++ adm.py
@@ -25,7 +25,20 @@ def unique_values_columns(data1:pd.DataFrame,name:str):
     else:
         print("在DataFrame中找不到该列")
         print("列名:",data.columns)
-      
+
+def unique_value_column_back(data1:pd.DataFrame,name:str):
+    data=jiancha(data1)
+    # 检测 参数name 是否为空
+    if not name:
+        print("提供的列名为空")
+        print("列名:",data.columns)
+    if name in data.columns:
+        unique_values = data[name].unique()
+        return(unique_values)
+    else:
+        print("在DataFrame中找不到该列")
+        print("列名:",data.columns)
+
 def year_back(data1:pd.DataFrame,name:str=None):
     #生成年份列表，可添加额外列名。
     year_list = [str(year) for year in range(1980, 2021 + 1)]
@@ -34,14 +47,15 @@ def year_back(data1:pd.DataFrame,name:str=None):
     return(year_list)
         
 
-
-def show_result_line(data:pd.DataFrame,name:str):
-    Country_list=data.filter(items=["Country"])
-    Country_list= Country_list.reset_index(drop=True)
-    Country_list["Country"] = Country_list["Country"].str.strip()
-    weizhi_list=Country_list[Country_list["Country"] == name].index.tolist()
-    weizhi=int(weizhi_list[0])
-    print(weizhi)
+def show_result_line(data:pd.DataFrame,name:str,title_name:str=None,X_name:str=None,Y_name:str=None,x_size:int=None,y_size:int=None):
+    data['Country'] = data['Country'].str.strip()
+    data = data.reset_index(drop=True)
+    country_indices = data[data['Country']== name].index
+    if country_indices.empty:
+        print("未找到名为{name}的国家数据")
+        return
+    weizhi=int(country_indices[0])
+    print()
     linshi=year_back(data,)
 
     selected_data = data.filter(items=linshi)
@@ -50,18 +64,70 @@ def show_result_line(data:pd.DataFrame,name:str):
     data2 = data.filter(items=linshi)
     
     result = {
-        '年份': linshi,
-        'Value': values
+        'x': linshi,
+        'y': values
     }
     result1 = pd.DataFrame(result)
-    plt_name=Country_list.iloc[0,0]
-    plt.figure(figsize=(30, 10))
-    sns.lineplot(x='年份', y='Value', data=result1)
-    plt.title(plt_name)
-    plt.xlabel('year')
-    plt.ylabel('value')
+
+    if x_size==None:
+        x_size=30
+    if y_size==None:
+        y_size=10
+    plt.figure(figsize=(x_size, y_size))
+    sns.lineplot(x='x', y='y', data=result1)
+    if title_name==None:
+        title_name=name
+    plt.title(title_name)
+    if X_name==None:
+        X_name="Year"
+    plt.xlabel(X_name)
+    if Y_name==None:
+        Y_name="value"
+    plt.ylabel(Y_name)
     ax = plt.gca()
     ax.invert_yaxis()
     #fig = plt.gcf()
     #fig.savefig('output.png')
-    #plt.show()
+    plt.show()
+
+
+def sum_year_data(data:pd.DataFrame,year1:int=None,region_num:int=None,region:str=None):
+    region_data=unique_value_column_back(data,'Region')#从给的dataframe中取独特值
+    state_group=data.groupby("Region")#按照地区分组
+    if region == None:
+        if region_num==None:
+            region_num=0
+    state_1=state_group.get_group(region_data[region_num])#取出需要的地区的相关表   
+    year_num=year_back(state_1)
+    year_num_series = pd.Series(year_num)
+    #print(year_num_series)
+    year=str(year1)
+    if year1 is None:
+        year=str(year_num[0])
+    if not year_num_series.isin([year]).any() :
+        print("给出的年份不在列表中")
+        return
+    sum_by_region=state_1[year].sum()
+    return(sum_by_region)
+    #mean_by_region=state_1[year_back(state_1)[0]].mean()
+
+
+def mean_year_data(data:pd.DataFrame,year1:int=None,region_num:int=None,region:str=None):
+    region_data=unique_value_column_back(data,'Region')#从给的dataframe中取独特值
+    state_group=data.groupby("Region")#按照地区分组
+    if region == None:
+        if region_num==None:
+            region_num=0
+    state_1=state_group.get_group(region_data[region_num])#取出需要的地区的相关表   
+    year_num=year_back(state_1)
+    year_num_series = pd.Series(year_num)
+    #print(year_num_series)
+    year=str(year1)
+    if year1 is None:
+        year=str(year_num[0])
+    if not year_num_series.isin([year]).any() :
+        print("给出的年份不在列表中")
+        return
+    mean_by_region=state_1[year].mean()
+    return(mean_by_region)
+    
--- main.py
+++ main.py
@@ -9,8 +9,13 @@ column_names = ['col1', 'col2', '']
 row_names = ['']
 #可能需要去重
 
-data = pd.read_csv(file_path,na_values=['--'])
-data_filled_with_zero = data.fillna(0)
+data = pd.read_csv(file_path)
+num_col=data.columns[2:]
+for col in num_col:
+    data[col]=pd.to_numeric(data[col],errors="coerce") #coerce返回NAN raise返回异常 ignore忽略
+
+data = data.fillna(0)
+#data.to_csv('fix.csv', index=False)
 
 #ad.unique_values_columns(data,'Features')  #找出表中列的唯一值
 loss_data=data[data['Features'] == 'distribution losses ']
@@ -18,9 +23,18 @@ imports_data=data[data['Features'] == 'imports ']
 exports_data=data[data['Features'] == 'exports ']
 net_generation_data=data[data['Features'] == 'net generation']
 
-ad.show_result_line(net_generation_data,"Afghanistan")
+#ad.show_result_line(net_generation_data,"Afghanistan")
 #ad.show_result_line(exports_data,"Afghanistan")
 #ad.show_result_line(imports_data,"Afghanistan")
 #ad.show_result_line(loss_data,"Afghanistan")
 
 
+
+#region_data=ad.unique_value_column_back(loss_data,'Region')
+#state_group=loss_data.groupby("Region")
+#state_1=state_group.get_group(region_data[0])
+
+#sum_by_region=state_1[ad.year_back(state_1)[0]].sum()
+#mean_by_region=state_1[ad.year_back(state_1)[0]].mean()
+print(ad.sum_year_data(loss_data,1999))
+print(ad.mean_year_data(loss_data,1999))
