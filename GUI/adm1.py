f6f259eef8b33283e8745d16af0b2151547d1133
adminliu-main <admshao.24@outlook.com>
2025-04-20 20:45:05 +0800

    任务1完成

--- GUI/adm1.py
+++ GUI/adm1.py
@@ -52,6 +52,17 @@ def aprint(contents:str):
     if language==0:
         print(f"发生错误：{contents}")
     
+def unique_value_column_back(data:pd.DataFrame,name:str):
+    if not name:
+        print("提供的列名为空")
+        print("列名:",data.columns)
+    if name in data.columns:
+        unique_values = data[name].unique()
+        return(unique_values)
+    else:
+        print("在DataFrame中找不到该列")
+        print("列名:",data.columns)
+
 def read_preprocessing(file_path:str):
     initialize(file_path)
     try:
@@ -75,3 +86,71 @@ def read_preprocessing(file_path:str):
 def years1():
     years = [str(year) for year in range(1980, 2022)]
     return(years)
+
+def years2(start:int,stop:int):
+    years = [str(year) for year in range(start, stop+1)]
+    return(years)
+   
+def year_chart(data:pd.DataFrame,selected_countries:list,selected_features:str):
+    years=years1()
+    #data=data[data['Features'] == selected_features]
+    selected_data = data[data['Country'].isin(selected_countries)][['Country'] + years]
+    title_name=selected_features.capitalize()+" Trend (1980-2021)"
+    plt_show(selected_data,years,(12,6),title_name,"Years",selected_features.capitalize())
+
+def year_chartbar(data:pd.DataFrame,selected_countries:list,years):
+    #data=data[data['Features'] == selected_features]
+    selected_data = data[data['Country'].isin(selected_countries)][['Country'] + years]
+    title_name=" Trend (1980-2021)"
+    plt_showbar(selected_data,years,(12,6),title_name,"Years","Value")
+
+
+def plt_show(data:pd.DataFrame,x_data:list,figsize1,title:str,x_name:str,y_name:str):
+    plt.figure(figsize=figsize1)
+
+    # 绘制每个国家的折线图
+    for index, row in data.iterrows():
+        country = row['Country']
+        plt.plot(x_data, row[x_data], label=country)
+
+    # 添加标题、标签和图例
+    plt.title(title)
+    plt.xlabel(x_name)
+    plt.ylabel(y_name)
+    plt.legend()
+
+    # 设置 x 轴标签旋转角度
+    plt.xticks(rotation=45)
+
+    # 显示图形
+    plt.show()
+
+def plt_showbar(data:pd.DataFrame,x_data:list,figsize1,title:str,x_name:str,y_name:str):
+    plt.figure(figsize=figsize1)
+    ziti = 'STHeiti'
+    plt.rcParams['font.family'] = ziti
+    plt.bar(data['Country'], data['Average Generation'])
+
+    # 添加标题、标签和图例
+    plt.title(title)
+    plt.xlabel(x_name)
+    plt.ylabel(y_name)
+    plt.legend()
+
+    # 设置 x 轴标签旋转角度
+
+    # 显示图形
+    plt.show()
+
+def mean_year_data(data:pd.DataFrame,year_start:int=None,year_stop:int=None,region_num:int=None,region:str=None):
+    data1=[]
+    data2=[]
+    data2 = pd.DataFrame(data2)
+    years=years2(year_start,year_stop)
+    data1 = data[['Country'] + years]
+    data2['Country']=data['Country'] 
+    data2['Average Generation'] = data1[years].mean(axis=1)
+    return(data2,years)
+
+ 
+
--- GUI/main1.py
+++ GUI/main1.py
@@ -1,9 +1,29 @@
 import pandas as pd
 import numpy as np
-import GUI.adm1 as ad
+import adm1 as ad
 import matplotlib.pyplot as plt
 import seaborn as sns
-language=1
 
 data = ad.read_preprocessing('GlobalElectricityStatistics.csv')
+loss_data=data[data['Features'] == 'distribution losses ']
+imports_data=data[data['Features'] == 'imports ']
+exports_data=data[data['Features'] == 'exports ']
+net_generation_data=data[data['Features'] == 'net generation']
 selected_countries = ['China', 'United States', 'India', 'Russia', 'Japan']
+
+#ad.year_chart(data,selected_countries)
+#ad.year_chart(data,selected_countries)
+#ad.year_chart(data,selected_countries)
+#ad.year_chart(data,selected_countries)
+
+data1=ad.mean_year_data(net_generation_data,2000,2021)[0]
+selected_data = data1[data1['Country'].isin(selected_countries)]
+#ad.plt_showbar(selected_data,selected_countries,(12,6),"2000 - 2021 年选定国家平均年发电量比较","国家","平均年发电量")
+
+max_avg_country = data1.loc[data1['Average Generation'].idxmax()]
+min_avg_country = data1.loc[data1['Average Generation'].idxmin()]
+
+print("\n该时期最高平均发电量的国家：")
+print(max_avg_country[['Country', 'Average Generation']])
+print("\n该时期最低平均发电量的国家：")
+print(min_avg_country[['Country', 'Average Generation']])
